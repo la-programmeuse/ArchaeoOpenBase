6600ca7d2d3655cd8bd9bc555d369f33ef5e1c9d
la-programmeuse <la.programmeuse@gmail.com>
2026-02-07 10:49:01 +0100

    viewpost

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
-import pymongo
+from pymongo import MongoClient
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 app = Flask("ArchaeoOpenBase")
+
+mongo = os.getenv('MONGO_URI')
+client = MongoClient(mongo)
+db = client.get_database("ArchaeoOpenBase")
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/viewpost")
+def viewpost():
+    posts_data=list(db['annonce'].find({}))
+    return render_template("viewpost.html", posts = posts_data)
+
 
 app.run(host ='0.0.0.0', port=81)
